29cda7ef4bb426deaaefd0d05da7186adf54f11f
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-04-20 17:08:08 +0100

    Add Chapter 9 content

--- the-book/ch9/README.md
+++ the-book/ch9/README.md
@@ -1,3 +1,6 @@
 # Chapter 9 - Error Handling
 
-- TODO
+- Unrecoverable errors (`panic!`); and,
+- Recoverable errors (`Result<T, E>`).
+
+Note, that exceptions don't exist in Rust - you won't find the try/except pattern, but will instead find functions that return `Result<T, E>` enums that achieve the same thing when handled using pattern matching.
--- the-book/ch9/src/main.rs
+++ the-book/ch9/src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+// Taken from Chapter 9 of The Rust Programming Language
+
+pub struct SomeValue {
+    value: i32,
 }
+
+impl SomeValue {
+    pub fn new(value: i32) -> SomeValue {
+        if value < 0 || value > 100 {
+            panic!("Value must be between 0 and 100, got {}.", value);  // unrecoverable
+        }
+
+        SomeValue { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+fn process_value(v: &SomeValue) -> Result<i32, &str> { // recoverable
+    if 2 * v.value > 100 {
+        return Err("2 x v > 100 - don't like this");
+    }
+    Ok(2 * v.value)
+}
+
+fn main() {
+    let sv0 = SomeValue::new(42);
+    let psv0 = match process_value(&sv0) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("setting output to 0 - {e}");
+            0
+        }
+    };
+    println!("process_value(..) = {psv0}");
+    println!("process_value(..) = {}", process_value(&sv0).unwrap());
+
+    let sv1 = SomeValue::new(52);
+    let psv1 = match process_value(&sv1) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("setting output to 0 - {e}");
+            0
+        }
+    };
+    println!("process_value(..) = {psv1}");
+    println!("process_value(..) = {}", process_value(&sv1).expect("nope: "));
+}
+
+// process_value(..) = 84
+// process_value(..) = 84
+// setting output to 0 - 2 x v > 100 - don't like this
+// process_value(..) = 0
+// thread 'main' panicked at src/main.rs:49:60:
+// nope: : "2 x v > 100 - don't like this"
+// note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
