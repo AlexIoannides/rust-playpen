61a164bd281d0b490a335daa3b4ca9c419876d89
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-04-23 23:47:52 +0100

    Add Chapter 17 content

---
+++ the-book/ch17/README.md
@@ -0,0 +1,3 @@
+# Chapter 17 - Elements of Object-Oriented Programming
+
+Moving beyond structs as objects and looking at how traits ad dynamic dispatcg can be used to specify types that implement a specific interface.
--- the-book/ch17/src/main.rs
+++ the-book/ch17/src/main.rs
@@ -1,5 +1,46 @@
 // Taken from Chapter 17 of The Rust Programming Language
+// --> elements of object-oriented programming in Rust
 
 fn main() {
-    println!("Hello, world!");
+    // dynamic dispatch based on trait implementation (analagous to interface implementation)
+    let me = Person::new(String::from("Alex"), String::from("Ioannides"));
+    let rabbit = Pet::new(String::from("Kiki"));
+    meet(&me);
+    meet(&rabbit);
+}
+
+fn meet(e: &dyn Greeting) {
+    e.greet();
+}
+
+trait Greeting {
+    fn greet(&self);
+}
+
+struct Person(String, String);
+
+impl Person {
+    fn new(first_name: String, second_name: String) -> Person {
+        Person(first_name, second_name)
+    }
+}
+
+impl Greeting for Person {
+    fn greet(&self) {
+        println!("Hello, said {} {}", self.0, self.1);
+    }
+}
+
+struct Pet(String);
+
+impl Pet {
+    fn new(name: String) -> Pet {
+        Pet(name)
+    }
+}
+
+impl Greeting for Pet {
+    fn greet(&self) {
+        println!("Hello, squeaked {}", self.0);
+    }
 }
